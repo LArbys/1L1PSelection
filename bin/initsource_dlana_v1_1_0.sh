26ed2fbc28fe084c7f532071d885548878edf967
Taritree Wongjirad <taritree@mit.edu>
2020-04-29 13:20:07 -0500

    save scripts

---
+++ bin/initsource_dlana_v1_1_0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+unsetup ubdl
+ups list -aK+ dllee_unified
+ups list -aK+ ubdlana
+ups list -aK+ ubMPIDnet
+setup dllee_unified v1_0_6 -q e17:prof
+source $DLLEE_UNIFIED_BASEDIR/configure.sh
+setup ubdlana v1_1_0 -q e17:prof
+
+export
+
+
